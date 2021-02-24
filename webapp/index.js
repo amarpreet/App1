494ed13c49e6a02cf746ca60b338ed1101f4028e
Singh <Amarpreet.Singh@itelligencegroup.com>
2021-02-24 18:21:07 +0300

    Initial commit

---
+++ webapp/index.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+ "sap/ui/core/mvc/XMLView"
+], function (XMLView) {
+	"use strict";
+
+
+	XMLView.create({
+		viewName: "sap.ui.demo.walkthrough.view.App"
+	}).then(function (oMyView) {
+		oMyView.placeAt("content");
+	});
+	
+});
